f240dea03f11d28bf73edd6217e2c65e5cba596c
Rafiq Premji <rafiq@prajnaconcepts.com>
2017-04-25 21:51:36 -0600

    more layout adjustments, more upgrades/placeholders

--- app.js
+++ app.js
@@ -16,16 +16,17 @@ new Vue({
     el: '#game',
     data: {
         crackers: 0,
+        totalCrackers: 0,
         clicks: 0,
         cps: 0,
         clickPower: 1,
 
         buildings: [
-            { name: 'Finger', baseCost: 15, baseCps: 0.1, description: 'Growing extra fingers will allow you to click more often.', owned: 0 },
-            { name: 'Toddler', baseCost: 100, baseCps: 1, description: "These toddlers will eat crackers if you tell them they're cookies.", owned: 0 },
-            { name: 'Kosher Bakery', baseCost: 1100, baseCps: 8, description: "These guys seem to know what they're doing.", owned: 0 },
-            { name: 'Non-Kosher Bakery', baseCost: 12000, baseCps: 47, description: "These guys don't follow the rules!", owned: 0 },
-            { name: 'Tea Club', baseCost: 130000, baseCps: 260, description: "These people LOVE crackers with their tea", owned: 0 },
+            { name: 'Finger', baseCost: 15, baseCps: 0.1, description: 'Growing extra fingers will allow you to click more often.', showAt: 0, owned: 0 },
+            { name: 'Toddler', baseCost: 100, baseCps: 1, description: "These toddlers will eat crackers if you tell them they're cookies.", showAt: 0, owned: 0 },
+            { name: 'Kosher Bakery', baseCost: 1100, baseCps: 8, description: "These guys seem to know what they're doing.", showAt: 15, owned: 0 },
+            { name: 'Non-Kosher Bakery', baseCost: 12000, baseCps: 47, description: "These guys don't follow the rules!", showAt: 100, owned: 0 },
+            { name: 'Tea Club', baseCost: 130000, baseCps: 260, description: "These people LOVE crackers with their tea", showAt: 1100, owned: 0 },
         ],
 
         upgrades: [
@@ -33,6 +34,11 @@ new Vue({
             { type: 'Cracker', name: 'Store Brand Crackers', needed: 50000, cost: 999999, multiplier: 1.01, description: 'Meh', active: false },
             { type: 'Cracker', name: 'Fancy Store Crackers', needed: 250000, cost: 5000000, multiplier: 1.01, description: 'Ok I guess', active: false },
             { type: 'Cracker', name: 'Rye Crackers', needed: 500000, cost: 10000000, multiplier: 1.01, description: 'Better than cardboard', active: false },
+            // 2.5M
+            // 5M
+            // 25M
+            // 50M
+            // etc
 
             // finger (cursor)
             { type: 'Finger', name: 'Double Tap', needed: 1, cost: 100, multiplier: 2, description: 'Doubles your finger clicks', active: false },
@@ -41,30 +47,52 @@ new Vue({
             { type: 'Finger', name: 'Middle Finger', needed: 20, cost: 100000, addition: 0.1, description: 'Put that thing away.', active: false },
             { type: 'Finger', name: 'Double Middle Finger', needed: 40, cost: 10000000, addition: 0.5, description: 'Put those away.', active: false },
             { type: 'Finger', name: 'Extra Middle Fingers', needed: 80, cost: 100000000, addition: 5, description: "Now that's just rude.", active: false },
+            { type: 'Finger', name: 'Super Finger', needed: 120, cost: 1000000000, addition: 50, description: "No decorum at all.", active: false },
+            { type: 'Finger', name: 'Super Duper Finger', needed: 160, cost: 10000000000, addition: 500, description: "...", active: false },
+            { type: 'Finger', name: 'Mega Finger', needed: 200, cost: 10000000000000, addition: 5000, description: "...!?...", active: false },
+            // 240
+            // 280
+            // 320
 
             // toddler (grandma)
             { type: 'Toddler', name: 'Timeout', needed: 1, cost: 1000, multiplier: 2, description: 'These toddlers need to learn some discipline', active: false },
             { type: 'Toddler', name: 'Daycare', needed: 5, cost: 5000, multiplier: 2, description: 'Finally, some me time!', active: false },
             { type: 'Toddler', name: 'Play Date', needed: 25, cost: 50000, multiplier: 2, description: 'An active social life is good for productivity', active: false },
             { type: 'Toddler', name: 'ADHD Meds', needed: 50, cost: 5000000, multiplier: 2, description: "That's better...", active: false },
+            { type: 'Toddler', name: 'Jack', needed: 100, cost: 500000000, multiplier: 2, description: "You like that movie?", active: false },
+            // 150
+            // 200
+            // 250
 
             // kosher bakery (farm)
             { type: 'Kosher Bakery', name: 'Torah', needed: 1, cost: 11000, multiplier: 2, description: 'Got to follow the rules', active: false },
             { type: 'Kosher Bakery', name: 'Another Torah', needed: 5, cost: 55000, multiplier: 2, description: 'More to go around', active: false },
             { type: 'Kosher Bakery', name: 'Rabbi', needed: 25, cost: 550000, multiplier: 2, description: "This rabbi doesn't come cheap", active: false },
             { type: 'Kosher Bakery', name: 'Rabbi Council', needed: 50, cost: 55000000, multiplier: 2, description: "These rabbis don't come cheap", active: false },
+            { type: 'Kosher Bakery', name: 'Passover Bonus', needed: 100, cost: 5500000000, multiplier: 2, description: "What does that even mean?", active: false },
+            // 150
+            // 200
+            // 250
 
             // non-kosher bakery (mine)
             { type: 'Non-Kosher Bakery', name: 'Knockoff Torah', needed: 1, cost: 120000, multiplier: 2, description: "This doesn't seem legit...", active: false },
             { type: 'Non-Kosher Bakery', name: 'Another Knockoff Torah', needed: 5, cost: 600000, multiplier: 2, description: "??", active: false },
             { type: 'Non-Kosher Bakery', name: 'Fake Rabbi', needed: 25, cost: 6000000, multiplier: 2, description: "I don't think his beard is real", active: false },
             { type: 'Non-Kosher Bakery', name: 'Fake Rabbi Council', needed: 50, cost: 600000000, multiplier: 2, description: "I have a bad feeling about this", active: false },
+            { type: 'Non-Kosher Bakery', name: 'Fake Passover Bonus', needed: 100, cost: 60000000000, multiplier: 2, description: "What does that even mean?!", active: false },
+            // 150
+            // 200
+            // 250
 
             // tea club (factory)
             { type: 'Tea Club', name: 'Strumpets', needed: 1, cost: 1300000, multiplier: 2, description: "What are we, savages?", active: false },
             { type: 'Tea Club', name: 'Valet', needed: 5, cost: 6500000, multiplier: 2, description: "I'm too rich to park my car", active: false },
             { type: 'Tea Club', name: 'Servant', needed: 25, cost: 65000000, multiplier: 2, description: "It's not technically slavery!", active: false },
             { type: 'Tea Club', name: 'Free trade tea', needed: 50, cost: 6500000000, multiplier: 2, description: "I feel so much better about this", active: false },
+            { type: 'Tea Club', name: 'Slavery tea', needed: 100, cost: 650000000000, multiplier: 2, description: "Suffering tastes better", active: false },
+            // 150
+            // 200
+            // 250
         ],
     },
     computed: {
@@ -75,6 +103,7 @@ new Vue({
         crackerClick: function () {
             this.clicks += this.clickPower;
             this.crackers += this.clickPower;
+            this.totalCrackers += this.clickPower;
         },
         recalculateClickPower: function () {
             this.clickPower = this.upgradeMultiplier('Finger') + this.upgradeAddition();
@@ -96,6 +125,9 @@ new Vue({
         },
 
         // buildings
+        showBuilding: function (building) {
+            return this.totalCrackers >= building.showAt;
+        },
         buyBuilding: function (building) {
             if (this.crackers >= this.buildingCost(building)) {
                 this.crackers -= this.buildingCost(building);
@@ -181,6 +213,7 @@ new Vue({
         // tick
         tick: function () {
             this.crackers += this.cps / 10;
+            this.totalCrackers += this.cps / 10;
         }
     },
     mounted: function () {
