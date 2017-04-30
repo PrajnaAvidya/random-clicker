16ec594ae9281943d5e5b38a7e68d2be81addaa9
Rafiq Premji <rafiq@prajnaconcepts.com>
2017-04-30 13:18:37 -0600

    fix NaN issue in round/currency

--- src/game.vue
+++ src/game.vue
@@ -620,38 +620,44 @@
         },
         filters: {
             round: function (value) {
+                if (isNaN(value)) {
+                    return 0;
+                }
                 if (value < 10) {
                     return Number((value).toFixed(1));
                 } else if (value <= 999999) {
                     return Math.round(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
                 } else {
                     // display pretty formatted number
+                    let displayedValue = new Big(value);
                     let suffixes = ["", "", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion"];
-                    let index = Math.floor((value.e) / 3);
+                    let index = Math.floor((displayedValue.e) / 3);
                     if (index >= suffixes.length) {
                         // outside of range
                         return value.toExponential(3);
                     }
-                    let suffix = suffixes[Math.floor((value.e) / 3)];
-                    let displayedValue = new Big(value);
+                    let suffix = suffixes[Math.floor((displayedValue.e) / 3)];
                     let sigFig = (displayedValue.e % 3);
                     displayedValue.e = 3 + sigFig;
                     return displayedValue.div(1000).toPrecision(4 + sigFig) + ' ' + suffix;
                 }
             },
             currency: function (value) {
+                if (isNaN(value)) {
+                    return 0;
+                }
                 if (value <= 999999) {
                     return Math.round(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
                 } else {
                     // display pretty formatted number
+                    let displayedValue = new Big(value);
                     let suffixes = ["", "", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion"];
-                    let index = Math.floor((value.e) / 3);
+                    let index = Math.floor((displayedValue.e) / 3);
                     if (index >= suffixes.length) {
                         // outside of range
                         return value.toExponential(3);
                     }
-                    let suffix = suffixes[Math.floor((value.e) / 3)];
-                    let displayedValue = new Big(value);
+                    let suffix = suffixes[Math.floor((displayedValue.e) / 3)];
                     let sigFig = (displayedValue.e % 3);
                     displayedValue.e = 3 + sigFig;
                     return displayedValue.div(1000).toPrecision(4 + sigFig) + ' ' + suffix;
